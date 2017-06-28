a68793113b2862dc30584f3052868f0ba66d56a3
FHellmann <fhellmann93@googlemail.com>
2017-06-28 15:26:41 +0200

    Add first implementation for 3-layer neural network

--- src/3lnn.h
+++ src/3lnn.h
@@ -1,38 +1,91 @@
-/*
- * 3lnn.h
- *
- *  Created on: 28.06.2017
- *      Author: Hellmann Fabio
- */
-
+#include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <math.h>
+#include "mnist.h"
 
 using namespace std;
 
 enum LayerType {INPUT, HIDDEN, OUTPUT};
+enum ActFctType {SIGMOID, TANH, NONE};
 
 struct Node {
 	double bias;
 	double output;
-	int wcount;
-	vector<double*> weights;
+	vector<double> weights;
 };
 
 struct Layer {
-	int ncount;
+	const LayerType layerType;
+	const ActFctType actFctType;
 	vector<Node*> nodes;
+
+	Layer(const LayerType layerType, const ActFctType actFctType)
+	: layerType(layerType), actFctType(actFctType) {
+	}
 };
 
 struct Network {
-	int inpNodeSize;
-	int inpLayerSize;
-	int hidNodeSize;
-	int hidLayerSize;
-	int outNodeSize;
-	int outLayerSize;
+	double learningRate;
 	vector<Layer*> layers;
+
+	Network() : learningRate(0.5) {
+	}
 };
 
-Network *createNetwork(int size_of_input_vector,
-                       int number_of_nodes_in_hidden_layer,
-                       int number_of_nodes_in_output_layer);
+// PUBLIC
+Network *createNetwork(const int size_of_input_vector,
+		const int number_of_nodes_in_hidden_layer,
+		const int number_of_nodes_in_output_layer);
+
+void feedInput(Network* nn,
+		Vector *v);
+
+// PRIVATE - not needed for public access
+void initNetwork(Network* nn,
+		const int inpCount,
+		const int hidCount,
+		const int outCount);
+
+void initWeights(Network* nn,
+		const LayerType layerType);
+
+Layer* createLayer(const int nodeCount,
+		const int weightCount,
+		const LayerType layerType,
+		const ActFctType actFctType);
+
+Layer* getLayer(Network* nn,
+		const LayerType layerType);
+
+void calcLayer(Network* nn,
+		const LayerType layerType);
+
+void calcNodeOutput(Network* nn,
+		const LayerType layerType,
+		const int id);
+
+void activateNode(Network *nn,
+		const LayerType ltype,
+		const int id);
+
+void backPropagateNetwork(Network *nn,
+		const int targetClassification);
+
+void backPropagateOutputLayer(Network *nn,
+		const int targetClassification);
+
+void backPropagateHiddenLayer(Network *nn,
+		const int targetClassification);
+
+double getActFctDerivative(Network *nn,
+		const LayerType layerType,
+		double outVal);
+
+void updateNodeWeights(Network *nn,
+		const LayerType ltype,
+		const int id,
+		double error);
+
+int getNetworkClassification(Network *nn);
+
---
+++ src/mnist.h
@@ -0,0 +1,10 @@
+#include <vector>
+
+using namespace std;
+
+struct Vector {
+	int size;
+	vector<double> vals;
+};
+
+//Vector* getVectorFromImage(const MNIST_Image *img);
