3a8d0382390665085e99cf8ce90665ecde46b5e6
Stefan Büttner <buettner.stefan@web.de>
2017-07-04 10:44:51 +0200

    Add a main file and function.

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jul 4, 2017
+ *      Author: Stefan
+ */
+#include <string>
+#include <iostream>
+#include <opencv/cv.h>
+
+CvMat loadMINSTImages();
+
+int main (int argc, char* argv[]) {
+
+	CvMat img;
+	exit (EXIT_SUCCESS);
+}
+
+CvMat loadMINSTImages(string const& path) {
+
+
+}
