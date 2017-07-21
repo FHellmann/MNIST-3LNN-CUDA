33ddd46925008443d26d7859ecccc38fb00b5e54
Stefan Büttner <buettner.stefan@web.de>
2017-07-21 09:10:11 +0200

    Use sufficiently large CUDA block size.
    Methods like matrix multiplication don't work when the grid doesn't
    cover all used matrix sizes

--- src/NeuralNetworkCUDA.cu
+++ src/NeuralNetworkCUDA.cu
@@ -270,7 +270,7 @@ __host__ void NeuralNetworkCUDA::train(MNISTImageDataset const& images,
 	cudaMemset(trainingParams.bias3, 0.0, trainingParams.bias3_len * sizeof(float));
 
 	// Configure Grid, i.e. setup Blocks and Threads
-	dim3 numBlocks(2,2);
+	dim3 numBlocks(MATRIX_SIZE_DIVISOR, MATRIX_SIZE_DIVISOR);
 	dim3 threadsPerBlock(MATRIX_SIZE_DIVISOR, MATRIX_SIZE_DIVISOR);
 	cout << "Blocks:            (" << numBlocks.x << ", " << numBlocks.y << ")"
 			<< endl;
