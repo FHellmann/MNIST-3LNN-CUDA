55908ce38f0c2b565a72e3c1b8680cd5ee738001
FHellmann <fhellmann93@googlemail.com>
2017-07-21 20:33:08 +0200

    Code Refactoring

--- src/trainLindNet.cpp
+++ src/trainLindNet.cpp
@@ -32,28 +32,35 @@ int main(int argc, char* argv[]) {
 			"path", parser);
 
 	ValueArg<string> networkType("t", "networkType",
-			"The neural network type (sequentiell, parallel, cuda).", false, "sequentiell", "type", parser);
+			"The neural network type (sequentiell, parallel, cuda).", false,
+			"sequentiell", "type", parser);
 
 	SwitchArg distributed("d", "distributed",
 			"The neural network will be executed distributed.", parser, false);
 
 	ValueArg<int> inputLayerNodes("", "inputNodes",
-			"The amount of input nodes.", false, 28*28, "inputLayerNodes", parser);
+			"The amount of input nodes.", false, 28 * 28, "inputLayerNodes",
+			parser);
 
 	ValueArg<int> hiddenLayerNodes("", "hiddenNodes",
-			"The amount of hidden nodes.", false, 20, "hiddenLayerNodes", parser);
+			"The amount of hidden nodes.", false, 20, "hiddenLayerNodes",
+			parser);
 
 	ValueArg<int> outputLayerNodes("", "outputNodes",
-			"The amount of output nodes.", false, 10, "outputLayerNodes", parser);
+			"The amount of output nodes.", false, 10, "outputLayerNodes",
+			parser);
 
 	ValueArg<double> learningRate("l", "learningRate",
-			"The learning rate of the neural network.", false, 0.2, "learningRate", parser);
+			"The learning rate of the neural network.", false, 0.2,
+			"learningRate", parser);
 
 	ValueArg<double> trainingErrorThreshold("", "trainingError",
-			"The training error when the neural network should quit work if the value is reached.", false, 0.04, "trainingError", parser);
+			"The training error when the neural network should quit work if the value is reached.",
+			false, 0.04, "trainingError", parser);
 
 	ValueArg<double> maxDerivation("", "derivation",
-			"The max derivation between to erros after two train samples where proceed.", false, 0.005, "maxDerivation", parser);
+			"The max derivation between to erros after two train samples where proceed.",
+			false, 0.005, "maxDerivation", parser);
 
 	try {
 		parser.parse(argc, argv);
@@ -75,28 +82,38 @@ int main(int argc, char* argv[]) {
 	NeuralNetwork* lindNet = nullptr;
 
 	string networkTypeSelection = networkType.getValue();
-	if(networkTypeSelection.compare("parallel") == 0) {
-		lindNet = new NeuralNetworkParallel(inputLayerNodes.getValue(), hiddenLayerNodes.getValue(), outputLayerNodes.getValue(), learningRate.getValue());
+	if (networkTypeSelection.compare("parallel") == 0) {
+		lindNet = new NeuralNetworkParallel(inputLayerNodes.getValue(),
+				hiddenLayerNodes.getValue(), outputLayerNodes.getValue(),
+				learningRate.getValue());
 		cout << "Neural Network - Parallel" << endl;
 	} else if (networkTypeSelection.compare("cuda") == 0) {
-		lindNet = new NeuralNetworkCUDA(inputLayerNodes.getValue(), hiddenLayerNodes.getValue(), outputLayerNodes.getValue(), learningRate.getValue());
+		lindNet = new NeuralNetworkCUDA(inputLayerNodes.getValue(),
+				hiddenLayerNodes.getValue(), outputLayerNodes.getValue(),
+				learningRate.getValue());
 		cout << "Neural Network - CUDA" << endl;
 	} else {
-		lindNet = new NeuralNetwork(inputLayerNodes.getValue(), hiddenLayerNodes.getValue(), outputLayerNodes.getValue(), learningRate.getValue());
+		lindNet = new NeuralNetwork(inputLayerNodes.getValue(),
+				hiddenLayerNodes.getValue(), outputLayerNodes.getValue(),
+				learningRate.getValue());
 		cout << "Neural Network - Sequentiell" << endl;
 	}
 
-	if(distributed.isSet()) {
+	if (distributed.isSet()) {
 		lindNet = new NeuralNetworkDistributed(argc, argv, *lindNet);
 		cout << "# !!! Distributed !!! #" << endl;
 	}
 
 	// Do some training.
-	std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
-	lindNet->train(trainingImages, trainingLabels, trainingErrorThreshold.getValue(), maxDerivation.getValue());
-	std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+	std::chrono::high_resolution_clock::time_point start =
+			std::chrono::high_resolution_clock::now();
+	lindNet->train(trainingImages, trainingLabels,
+			trainingErrorThreshold.getValue(), maxDerivation.getValue());
+	std::chrono::high_resolution_clock::time_point end =
+			std::chrono::high_resolution_clock::now();
 
-	std::chrono::duration<double> sec = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+	std::chrono::duration<double> sec = std::chrono::duration_cast<
+			std::chrono::duration<double>>(end - start);
 	cout << "Taining done in " << sec.count() << "s" << endl;
 
 	// Save the trained net.
