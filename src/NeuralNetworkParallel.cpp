55bc238872875e1a6807046b7103d3337f7c7311
FHellmann <fhellmann93@googlemail.com>
2017-07-21 19:57:44 +0200

    Add comments; Code Refactoring

--- src/NeuralNetworkParallel.cpp
+++ src/NeuralNetworkParallel.cpp
@@ -2,12 +2,16 @@
 
 using namespace std;
 
-NeuralNetworkParallel::NeuralNetworkParallel(const int inpCount, const int hidCount,
-		const int outCount, const double _learningRate) {
+NeuralNetworkParallel::NeuralNetworkParallel(const int inpCount,
+		const int hidCount, const int outCount, const double _learningRate) {
 	learningRate = _learningRate;
 	layers.push_back(new LayerParallel(inpCount, 0, INPUT, NONE, nullptr));
-	layers.push_back(new LayerParallel(hidCount, inpCount, HIDDEN, SIGMOID, layers.back()));
-	layers.push_back(new LayerParallel(outCount, hidCount, OUTPUT, SIGMOID, layers.back()));
+	layers.push_back(
+			new LayerParallel(hidCount, inpCount, HIDDEN, SIGMOID,
+					layers.back()));
+	layers.push_back(
+			new LayerParallel(outCount, hidCount, OUTPUT, SIGMOID,
+					layers.back()));
 
 	for (int l = 0; l < layers.size() - 1; l++) { // leave out the output layer
 		Layer* layer = layers.at(l);
@@ -16,12 +20,12 @@ NeuralNetworkParallel::NeuralNetworkParallel(const int inpCount, const int hidCo
 
 			for (int j = 0; j < node->weights.size(); j++) {
 				node->weights[j] = 0.7 * (rand() / (double) (RAND_MAX));
-				if(j % 2)
+				if (j % 2)
 					node->weights[j] = -node->weights[j]; // make half of the weights negative
 			}
 
 			node->bias = rand() / (double) (RAND_MAX);
-			if(i % 2)
+			if (i % 2)
 				node->bias = -node->bias; // make half of the bias weights negative
 		}
 	}
@@ -32,7 +36,9 @@ NeuralNetworkParallel::NeuralNetworkParallel(NeuralNetworkParallel const& net) :
 	layers.reserve(net.layers.size());
 	for (size_t i = 0; i < net.layers.size(); ++i) {
 		// Make a deep copy of the layers
-		layers.push_back(new LayerParallel(*dynamic_cast<LayerParallel*>(net.layers[i])));
+		layers.push_back(
+				new LayerParallel(
+						*dynamic_cast<LayerParallel*>(net.layers[i])));
 	}
 
 	// And set the previous layer in the new network.
@@ -47,25 +53,35 @@ NeuralNetworkParallel::NeuralNetworkParallel(NeuralNetworkParallel const& net) :
 	}
 }
 
-void mergeNeuralNetworks(NeuralNetworkParallel& omp_in, NeuralNetworkParallel& omp_out, NeuralNetworkParallel* reset) {
-	for(int l=0; l < omp_in.layers.size(); l++) {
+/**
+ * Merge the weights of omp_in into the omp_out network. The reset network is used
+ * to subtract the weights with the weights of omp_in. This results in only adding
+ * the delta weights to the weights of omp_out.
+ *
+ * @param omp_in The neural network with the weights to add to omp_out.
+ * @param omp_out The neural network to add the delta weights to.
+ * @param reset The neural network with the weights to subtract from omp_in weights.
+ */
+void mergeNeuralNetworks(NeuralNetworkParallel& omp_in,
+		NeuralNetworkParallel& omp_out, NeuralNetworkParallel* reset) {
+	for (int l = 0; l < omp_in.layers.size(); l++) {
 		NeuralNetwork::Layer* layerIn = omp_in.layers.at(l);
 		NeuralNetwork::Layer* layerOut = omp_out.layers.at(l);
 		NeuralNetwork::Layer* layerReset = reset->layers.at(l);
-		for(int n=0; n < layerIn->nodes.size(); n++) {
+		for (int n = 0; n < layerIn->nodes.size(); n++) {
 			NeuralNetwork::Layer::Node* nodeIn = layerIn->nodes.at(n);
 			NeuralNetwork::Layer::Node* nodeOut = layerOut->nodes.at(n);
 			NeuralNetwork::Layer::Node* nodeReset = layerReset->nodes.at(n);
-			for(int w=0; w < nodeIn->weights.size(); w++) {
-				nodeOut->weights.at(w) += nodeIn->weights.at(w) - nodeReset->weights.at(w);
+			for (int w = 0; w < nodeIn->weights.size(); w++) {
+				nodeOut->weights.at(w) += nodeIn->weights.at(w)
+						- nodeReset->weights.at(w);
 			}
 		}
 	}
 }
 
 double NeuralNetworkParallel::train(MNISTImageDataset const& images,
-		MNISTLableDataset const& labels,
-		double const training_error_threshold,
+		MNISTLableDataset const& labels, double const training_error_threshold,
 		double const max_derivation) {
 
 	bool needsFurtherTraining = true;
@@ -74,15 +90,17 @@ double NeuralNetworkParallel::train(MNISTImageDataset const& images,
 
 	NeuralNetworkParallel nnp_merge(*this);
 
-	#pragma omp parallel shared(needsFurtherTraining,error,newError)
+	// Split the work to n threads
+#pragma omp parallel shared(needsFurtherTraining,error,newError)
 	{
+		// Every thread creates an own copy of the neural network to work on
 		NeuralNetworkParallel nnp_local(*this);
 		int every_ten_percent = images.size() / 10;
 
-		while(needsFurtherTraining) {
+		while (needsFurtherTraining) {
 			size_t localErrCount = 0;
 
-			#pragma omp for
+#pragma omp for
 			for (size_t imgCount = 0; imgCount < images.size(); imgCount++) {
 				// Convert the MNIST image to a standardized vector format and feed into the network
 				nnp_local.feedInput(images[imgCount]);
@@ -105,27 +123,29 @@ double NeuralNetworkParallel::train(MNISTImageDataset const& images,
 				//}
 			}
 
-			#pragma omp atomic
-			newError += static_cast<double>(localErrCount) / static_cast<double>(images.size());
+#pragma omp atomic
+			newError += static_cast<double>(localErrCount)
+					/ static_cast<double>(images.size());
 
-			// merge network weights together
-			#pragma omp critical
+			// Merge network weights together
+#pragma omp critical
 			mergeNeuralNetworks(nnp_local, nnp_merge, &nnp_merge);
 
-			#pragma omp barrier
+#pragma omp barrier
 			if (newError < error) {
 				error = newError;
 			}
 
-			if(newError < training_error_threshold || newError > error + max_derivation) {
+			if (newError < training_error_threshold
+					|| newError > error + max_derivation) {
 				needsFurtherTraining = false;
-			}
-			else
+			} else
+				// Merge the global weights back to the local networks of each thread
 				mergeNeuralNetworks(nnp_merge, nnp_local, &nnp_merge);
 
-			#pragma omp barrier
+#pragma omp barrier
 
-			#pragma omp master
+#pragma omp master
 			{
 				//cout << " Error: " << newError * 100.0 << "%" << endl;
 
@@ -134,6 +154,7 @@ double NeuralNetworkParallel::train(MNISTImageDataset const& images,
 		}
 	}
 
+	// Merge the global weights into the real neural network
 	mergeNeuralNetworks(nnp_merge, *this, this);
 
 	//cout << endl;
@@ -141,9 +162,10 @@ double NeuralNetworkParallel::train(MNISTImageDataset const& images,
 	return error;
 }
 
-NeuralNetworkParallel::LayerParallel::LayerParallel(const int nodeCount, const int weightCount,
-		const LayerType _layerType, const ActFctType _actFctType, Layer* _previous) :
-			Layer(nodeCount, weightCount, _layerType, _actFctType, _previous) {
+NeuralNetworkParallel::LayerParallel::LayerParallel(const int nodeCount,
+		const int weightCount, const LayerType _layerType,
+		const ActFctType _actFctType, Layer* _previous) :
+		Layer(nodeCount, weightCount, _layerType, _actFctType, _previous) {
 }
 
 NeuralNetworkParallel::LayerParallel::LayerParallel(LayerParallel const& layer) :
--- src/NeuralNetworkParallel.h
+++ src/NeuralNetworkParallel.h
@@ -5,19 +5,29 @@
 #include <omp.h>
 #include <iostream>
 
-class NeuralNetworkParallel : public NeuralNetwork {
+class NeuralNetworkParallel: public NeuralNetwork {
 public:
-	NeuralNetworkParallel(const int inpCount, const int hidCount, const int outCount,
-			const double learningRate);
+	/**
+	 * Creates a new simple 3-layer neural network.
+	 *
+	 * @param inpCount The input layer node count.
+	 * @param hidCount the hidden layer node count.
+	 * @param outCount The output layer node count.
+	 * @param learningRate The learning rate of this neural network.
+	 */
+	NeuralNetworkParallel(const int inpCount, const int hidCount,
+			const int outCount, const double learningRate);
 
+	/**
+	 * Copy ctr.
+	 */
 	NeuralNetworkParallel(NeuralNetworkParallel const&);
 
 	double train(MNISTImageDataset const& images,
 			MNISTLableDataset const& labels,
-			double const training_error_threshold,
-			double const max_derivation);
+			double const training_error_threshold, double const max_derivation);
 
-	class LayerParallel : public Layer {
+	class LayerParallel: public Layer {
 	public:
 		LayerParallel(const int nodeCount, const int weightCount,
 				const LayerType layerType, const ActFctType actFctType,
