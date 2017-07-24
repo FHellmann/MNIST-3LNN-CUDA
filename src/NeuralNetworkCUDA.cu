319e5749ba1fb95eca398f10af4d60cd51008730
Stefan Büttner <buettner.stefan@web.de>
2017-07-24 14:38:07 +0200

    Make the images in GPUTrainingParams a matrix and prepare for sequential batch processing.

--- src/NeuralNetworkCUDA.cu
+++ src/NeuralNetworkCUDA.cu
@@ -33,16 +33,16 @@ struct Matrix {
 
 struct GPUTrainingParameters {
 	/* Training data. */
-	float* images;
-	float* labels;
+	/** Only a reference to the global image data on the GPU. */
+	Matrix images;
+	/** Only a reference to the global label data on the GPU. */
+	Matrix labels;
 
 	/* Training data parameters. */
-	size_t numExamples;
 	size_t numHiddenNodes;
-	size_t width;
-	size_t height;
-	// Number of images per training
+	/** Number of images per training. */
 	size_t batchSize;
+	float learningRate;
 
 	/* Weight matrices. */
 	Matrix W12;
@@ -60,9 +60,8 @@ struct GPUTrainingParameters {
 	NeuralNetwork::ActFctType activationFunction3;
 
 	/* Training parameters. */
-	float errorThreshold;
-	float maxDerivation;
-	float learningRate;
+//	float errorThreshold;
+//	float maxDerivation;
 
 	/* Temporary buffers, e.g. for back propagation. */
 	Matrix tmp3;
@@ -118,6 +117,11 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 __global__ void d_feed_forward(GPUTrainingParameters const);
 __global__ void d_back_propagate(GPUTrainingParameters const);
 
+__host__ GPUTrainingParameters initTrainingParams(NeuralNetwork& net,
+		size_t const batchSize, size_t const batchOffset, float* const d_images,
+		size_t const imageSize, float* const d_labels, size_t const labelSize);
+__host__ void freeTrainingParams(GPUTrainingParameters& params);
+
 __host__ void NeuralNetworkCUDA::train(MNISTImageDataset const& images,
 		MNISTLableDataset const& labels, double const training_error_threshold,
 		double const max_derivation) {
@@ -127,35 +131,168 @@ __host__ void NeuralNetworkCUDA::train(MNISTImageDataset const& images,
 	if (labels.size() <= 0)
 		return;
 
-	Layer* const inputLayer  = getLayer(INPUT);
-	Layer* const hiddenLayer = getLayer(HIDDEN);
-	Layer* const outputLayer = getLayer(OUTPUT);
-
-	GPUTrainingParameters trainingParams;
-	trainingParams.numExamples = images.size();
-	trainingParams.width = images.front().cols;
-	trainingParams.height = images.front().rows;
-	trainingParams.numHiddenNodes = hiddenLayer->nodes.size();
-	trainingParams.learningRate = learningRate;
-	trainingParams.errorThreshold = training_error_threshold;
-	trainingParams.maxDerivation = max_derivation;
-	trainingParams.batchSize = MATRIX_SIZE_DIVISOR;
-
 	cudaError_t err;
 
-	//
-	// Allocate cuda memory
-	//
 	size_t const singleImgPixCount = images.front().total();
 	size_t const allImgBufElements = singleImgPixCount * images.size();
 
-	// Images
-	err = cudaMalloc((void**) &trainingParams.images, allImgBufElements * sizeof(float));
+	//
+	// Convert the image data and labels into a float array.
+	//
+	float* fImgData = new float[allImgBufElements];
+	float* dst = fImgData;
+	for (cv::Mat const& img : images) {
+		for (uint8_t* src = img.datastart; src != img.dataend;) {
+			*(dst++) = static_cast<float>(*(src++));
+		}
+	}
+
+	float* flabels = new float[labels.size()];
+	dst = flabels;
+	for (uint8_t const& l : labels) {
+		*(dst++) = static_cast<float>(l);
+	}
+
+	//
+	// Copy all the images onto the GPU.
+	//
+	float* d_images = nullptr;
+	float* d_labels = nullptr;
+
+	err = cudaMalloc((void**) &d_images, allImgBufElements * sizeof(float));
+	assert(err == cudaSuccess);
+	err = cudaMalloc((void**) &d_labels, labels.size() * sizeof(float));
+	assert(err == cudaSuccess);
+	err = cudaMemcpy(d_images, fImgData, allImgBufElements * sizeof(float), cudaMemcpyHostToDevice);
+	assert(err == cudaSuccess);
+	err = cudaMemcpy(d_labels, flabels, labels.size() * sizeof(float), cudaMemcpyHostToDevice);
 	assert(err == cudaSuccess);
 
-	// Labels
-	err = cudaMalloc((void**) &trainingParams.labels, labels.size() * sizeof(float));
-	assert(err == cudaSuccess);
+	// Delete the image and label buffers on the host.
+	delete[] fImgData;
+	fImgData = nullptr;
+	delete[] flabels;
+	flabels = nullptr;
+
+
+	{
+		GPUTrainingParameters trainingParams = initTrainingParams(*this, 600, 0, d_images, singleImgPixCount, d_labels, 1);
+		cout << "Batch size: " << trainingParams.batchSize << endl;
+
+		// Configure Grid, i.e. setup Blocks and Threads
+		dim3 numBlocks(MATRIX_SIZE_DIVISOR, MATRIX_SIZE_DIVISOR);
+		dim3 threadsPerBlock(MATRIX_SIZE_DIVISOR, MATRIX_SIZE_DIVISOR);
+		cout << "Blocks:            (" << numBlocks.x << ", " << numBlocks.y << ")"
+				<< endl;
+		cout << "Threads per block: (" << threadsPerBlock.x << ", "
+				<< threadsPerBlock.y << ")" << endl;
+
+		// Call graphics card functions
+		d_feed_forward<<<numBlocks, threadsPerBlock>>>(trainingParams);
+		gpuErrchk( cudaPeekAtLastError() );
+		gpuErrchk( cudaDeviceSynchronize() );
+		d_back_propagate<<<numBlocks, threadsPerBlock>>>(trainingParams);
+		gpuErrchk( cudaPeekAtLastError() );
+		gpuErrchk( cudaDeviceSynchronize() );
+
+		//
+		// Retreive the data
+		//
+		float* W12 = new float[matrix_size(trainingParams.W12)];
+		float* W23 = new float[matrix_size(trainingParams.W23)];
+		float* bias2 = new float[matrix_size(trainingParams.bias2)];
+		float* bias3 = new float[matrix_size(trainingParams.bias3)];
+
+		// Copy it back to neural network data structure
+		err = cudaMemcpy(W12, trainingParams.W12.data, matrix_size(trainingParams.W12) * sizeof(float), cudaMemcpyDeviceToHost);
+		assert(err == cudaSuccess);
+		err = cudaMemcpy(W23, trainingParams.W23.data, matrix_size(trainingParams.W23) * sizeof(float), cudaMemcpyDeviceToHost);
+		assert(err == cudaSuccess);
+		err = cudaMemcpy(bias2, trainingParams.bias2.data, matrix_size(trainingParams.bias2) * sizeof(float), cudaMemcpyDeviceToHost);
+		assert(err == cudaSuccess);
+		err = cudaMemcpy(bias3, trainingParams.bias3.data, matrix_size(trainingParams.bias3) * sizeof(float), cudaMemcpyDeviceToHost);
+		assert(err == cudaSuccess);
+
+		//
+		// Copy the weight data into the c++ data structure.
+		//
+		Layer* const inputLayer  = getLayer(INPUT);
+		Layer* const hiddenLayer = getLayer(HIDDEN);
+		Layer* const outputLayer = getLayer(OUTPUT);
+		trainingParams.activationFunction2 = hiddenLayer->actFctType;
+		{
+			size_t k = 0;
+			for (size_t j = 0; j < hiddenLayer->nodes.size(); ++j) {
+				Layer::Node* node = hiddenLayer->nodes[j];
+				node->bias = bias2[j];
+				for (size_t i = 0; i < node->weights.size(); ++i) {
+					node->weights[i] = W12[k];
+					++k;
+				}
+			}
+		}
+
+		trainingParams.activationFunction3 = outputLayer->actFctType;
+		{
+			size_t k = 0;
+			for (size_t j = 0; j < outputLayer->nodes.size(); ++j) {
+				Layer::Node* node = outputLayer->nodes[j];
+				node->bias = bias3[j];
+				for (size_t i = 0; i < node->weights.size(); ++i) {
+					node->weights[i] = W23[k];
+					++k;
+				}
+			}
+		}
+
+		// Delete the host buffers
+		delete[] W12;
+		W12 = nullptr;
+		delete[] W23;
+		W23 = nullptr;
+		delete[] bias2;
+		bias2 = nullptr;
+		delete[] bias3;
+		bias3 = nullptr;
+
+		freeTrainingParams(trainingParams);
+	}
+
+	// Free the cuda buffers
+	cudaFree (d_images);
+	d_images = nullptr;
+	cudaFree (d_labels);
+	d_labels = nullptr;
+}
+
+__host__ GPUTrainingParameters initTrainingParams(NeuralNetwork& net, size_t const batchSize,
+		size_t const batchOffset, float* const d_images,
+		size_t const imageSize, float* const d_labels,
+		size_t const labelSize) {
+
+	NeuralNetwork::Layer* const inputLayer  = net.getLayer(NeuralNetwork::INPUT);
+	NeuralNetwork::Layer* const hiddenLayer = net.getLayer(NeuralNetwork::HIDDEN);
+	NeuralNetwork::Layer* const outputLayer = net.getLayer(NeuralNetwork::OUTPUT);
+
+	GPUTrainingParameters trainingParams;
+	trainingParams.numHiddenNodes = hiddenLayer->nodes.size();
+	//trainingParams.errorThreshold = training_error_threshold;
+	//trainingParams.maxDerivation = max_derivation;
+	trainingParams.batchSize = batchSize;
+	trainingParams.learningRate = net.learningRate;
+
+	// Set the image and labels matrices
+	trainingParams.images.rows = imageSize;
+	trainingParams.images.cols = batchSize;
+	trainingParams.images.layout = Matrix::COLUMN_MAJOR;
+	trainingParams.images.data = d_images + imageSize * batchOffset;
+
+	trainingParams.labels.rows = 1;
+	trainingParams.labels.cols = batchSize;
+	trainingParams.labels.layout = Matrix::COLUMN_MAJOR;
+	trainingParams.labels.data = d_labels + labelSize * batchOffset;
+
+	cudaError_t err;
 
 	// Storage for the first weight matrix
 	trainingParams.W12.rows = hiddenLayer->nodes.size();
@@ -213,23 +350,6 @@ __host__ void NeuralNetworkCUDA::train(MNISTImageDataset const& images,
 	err = cudaMalloc((void**) &trainingParams.tmp2.data, matrix_size(trainingParams.tmp2) * sizeof(float));
 	assert(err == cudaSuccess);
 
-	//
-	// Collect memory in RAM
-	//
-	float* imgData = new float[allImgBufElements];
-	float* dst = imgData;
-	for (cv::Mat const& img : images) {
-		for (uint8_t* src = img.datastart; src != img.dataend;) {
-			*(dst++) = static_cast<float>(*(src++));
-		}
-	}
-
-	float* flabels = new float[labels.size()];
-	dst = flabels;
-	for (uint8_t const& l : labels) {
-		*(dst++) = static_cast<float>(l);
-	}
-
 	float* W12 = new float[matrix_size(trainingParams.W12)];
 	float* W23 = new float[matrix_size(trainingParams.W23)];
 	float* bias2 = new float[matrix_size(trainingParams.bias2)];
@@ -242,7 +362,7 @@ __host__ void NeuralNetworkCUDA::train(MNISTImageDataset const& images,
 	{
 		size_t k = 0;
 		for (size_t j = 0; j < hiddenLayer->nodes.size(); ++j) {
-			Layer::Node* node = hiddenLayer->nodes[j];
+			NeuralNetwork::Layer::Node* node = hiddenLayer->nodes[j];
 			bias2[j] = node->bias;
 			for (size_t i = 0; i < node->weights.size(); ++i, ++k) {
 				W12[k] = node->weights[i];
@@ -254,7 +374,7 @@ __host__ void NeuralNetworkCUDA::train(MNISTImageDataset const& images,
 	{
 		size_t k = 0;
 		for (size_t j = 0; j < outputLayer->nodes.size(); ++j) {
-			Layer::Node* node = outputLayer->nodes[j];
+			NeuralNetwork::Layer::Node* node = outputLayer->nodes[j];
 			bias3[j] = node->bias;
 			for (size_t i = 0; i < node->weights.size(); ++i, ++k) {
 				W23[k] = node->weights[i];
@@ -265,10 +385,6 @@ __host__ void NeuralNetworkCUDA::train(MNISTImageDataset const& images,
 	//
 	// Copy data to graphics card
 	//
-	err = cudaMemcpy(trainingParams.images, imgData, allImgBufElements * sizeof(float), cudaMemcpyHostToDevice);
-	assert(err == cudaSuccess);
-	err = cudaMemcpy(trainingParams.labels, flabels, labels.size() * sizeof(float), cudaMemcpyHostToDevice);
-	assert(err == cudaSuccess);
 	err = cudaMemcpy(trainingParams.W12.data, W12, matrix_size(trainingParams.W12) * sizeof(float), cudaMemcpyHostToDevice);
 	assert(err == cudaSuccess);
 	err = cudaMemcpy(trainingParams.bias2.data, bias2, matrix_size(trainingParams.bias2) * sizeof(float), cudaMemcpyHostToDevice);
@@ -278,46 +394,15 @@ __host__ void NeuralNetworkCUDA::train(MNISTImageDataset const& images,
 	err = cudaMemcpy(trainingParams.bias3.data, bias3, matrix_size(trainingParams.bias3) * sizeof(float), cudaMemcpyHostToDevice);
 	assert(err == cudaSuccess);
 
-	delete[] imgData;
-	imgData = nullptr;
-	delete[] flabels;
-	flabels = nullptr;
+	delete[] W12;
+	delete[] W23;
+	delete[] bias2;
+	delete[] bias3;
 
-	// Configure Grid, i.e. setup Blocks and Threads
-	dim3 numBlocks(MATRIX_SIZE_DIVISOR, MATRIX_SIZE_DIVISOR);
-	dim3 threadsPerBlock(MATRIX_SIZE_DIVISOR, MATRIX_SIZE_DIVISOR);
-	cout << "Blocks:            (" << numBlocks.x << ", " << numBlocks.y << ")"
-			<< endl;
-	cout << "Threads per block: (" << threadsPerBlock.x << ", "
-			<< threadsPerBlock.y << ")" << endl;
+	return trainingParams;
+}
 
-	// Call graphics card functions
-	d_feed_forward<<<numBlocks, threadsPerBlock>>>(trainingParams);
-	gpuErrchk( cudaPeekAtLastError() );
-	gpuErrchk( cudaDeviceSynchronize() );
-	d_back_propagate<<<numBlocks, threadsPerBlock>>>(trainingParams);
-	gpuErrchk( cudaPeekAtLastError() );
-	gpuErrchk( cudaDeviceSynchronize() );
-
-	//
-	// Retreive the data
-	//
-
-	// Copy it back to neural network data structure
-	err = cudaMemcpy(W12, trainingParams.W12.data, matrix_size(trainingParams.W12) * sizeof(float), cudaMemcpyDeviceToHost);
-	assert(err == cudaSuccess);
-	err = cudaMemcpy(W23, trainingParams.W23.data, matrix_size(trainingParams.W23) * sizeof(float), cudaMemcpyDeviceToHost);
-	assert(err == cudaSuccess);
-	err = cudaMemcpy(bias2, trainingParams.bias2.data, matrix_size(trainingParams.bias2) * sizeof(float), cudaMemcpyDeviceToHost);
-	assert(err == cudaSuccess);
-	err = cudaMemcpy(bias3, trainingParams.bias3.data, matrix_size(trainingParams.bias3) * sizeof(float), cudaMemcpyDeviceToHost);
-	assert(err == cudaSuccess);
-
-	// Free the cuda buffers
-	cudaFree (trainingParams.images);
-	trainingParams.images = nullptr;
-	cudaFree (trainingParams.labels);
-	trainingParams.labels = nullptr;
+void freeTrainingParams(GPUTrainingParameters& trainingParams) {
 	cudaFree (trainingParams.W12.data);
 	trainingParams.W12.data = nullptr;
 	cudaFree (trainingParams.W23.data);
@@ -334,45 +419,6 @@ __host__ void NeuralNetworkCUDA::train(MNISTImageDataset const& images,
 	trainingParams.tmp3.data = nullptr;
 	cudaFree (trainingParams.tmp2.data);
 	trainingParams.tmp2.data = nullptr;
-
-	//
-	// Copy the weight data into the c++ data structure.
-	//
-	trainingParams.activationFunction2 = hiddenLayer->actFctType;
-	{
-		size_t k = 0;
-		for (size_t j = 0; j < hiddenLayer->nodes.size(); ++j) {
-			Layer::Node* node = hiddenLayer->nodes[j];
-			node->bias = bias2[j];
-			for (size_t i = 0; i < node->weights.size(); ++i) {
-				node->weights[i] = W12[k];
-				++k;
-			}
-		}
-	}
-
-	trainingParams.activationFunction3 = outputLayer->actFctType;
-	{
-		size_t k = 0;
-		for (size_t j = 0; j < outputLayer->nodes.size(); ++j) {
-			Layer::Node* node = outputLayer->nodes[j];
-			node->bias = bias3[j];
-			for (size_t i = 0; i < node->weights.size(); ++i) {
-				node->weights[i] = W23[k];
-				++k;
-			}
-		}
-	}
-
-	// Delete the host buffers
-	delete[] W12;
-	W12 = nullptr;
-	delete[] W23;
-	W23 = nullptr;
-	delete[] bias2;
-	bias2 = nullptr;
-	delete[] bias3;
-	bias3 = nullptr;
 }
 
 /* Matrix manipulation operations. */
@@ -399,14 +445,8 @@ __global__ void d_feed_forward(GPUTrainingParameters const params) {
 
 	PRINTF("d_feed_forward\n");
 
-	Matrix imgs;
-	imgs.rows = params.width * params.height;
-	imgs.cols = params.batchSize;
-	imgs.layout = Matrix::COLUMN_MAJOR;
-	imgs.data = params.images; // Global data pointer, column major, yields one image in each column vector.
-
 	d_set_bias(params.output2, params.bias2);
-	d_mul_add(params.output2, params.W12, imgs);
+	d_mul_add(params.output2, params.W12, params.images);
 	d_apply_activation(params.output2, params.activationFunction2);
 
 	d_set_bias(params.output3, params.bias3);
@@ -473,13 +513,8 @@ __device__ void d_back_propagate_hidden(GPUTrainingParameters const& params) {
 	d_apply_activation_derivative(params.output2, params.activationFunction2);
 	d_cwise_mul(params.tmp2, params.output2, params.tmp2);
 
-	Matrix images;
-	images.rows = params.width * params.height;
-	images.cols = params.batchSize;
-	images.layout = Matrix::COLUMN_MAJOR;
-	images.data = params.images;
-	images = d_matrix_transpose(images);
 
+	Matrix images = d_matrix_transpose(params.images);
 	d_mul_add(params.W12, params.tmp2, images);
 }
 
@@ -535,7 +570,7 @@ __device__ void d_fill_target_output(GPUTrainingParameters const& params, Matrix
 		return;
 	}
 
-	size_t const srcIdx = threadIdx.x + blockIdx.x * blockDim.x;
+	size_t const imgIdx = threadIdx.x + blockIdx.x * blockDim.x;
 	size_t const targetX = threadIdx.x + blockIdx.x * blockDim.x;
 	size_t const targetY = threadIdx.y + blockIdx.y * blockDim.y;
 
@@ -543,7 +578,7 @@ __device__ void d_fill_target_output(GPUTrainingParameters const& params, Matrix
 		return;
 	}
 
-	float const v = (threadIdx.y == params.labels[srcIdx]) ? 1.0f : 0.0f;
+	float const v = (threadIdx.y == d_matrix_get(params.labels, 1, imgIdx)) ? 1.0f : 0.0f;
 	d_matrix_set(targetOutput, targetY, targetX, v);
 }
 
